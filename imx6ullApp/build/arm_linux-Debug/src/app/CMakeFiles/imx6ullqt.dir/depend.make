63eb897784923e6c6a7a1a9e5598b3b4275a9a7c
baba5410 <2250509837@qq.com>
2026-04-18 17:35:12 +0800

    chore: 清理编译中间件，添加 .gitignore 忽略构建文件

--- imx6ullApp/build/arm_linux-Debug/src/app/CMakeFiles/imx6ullqt.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for imx6ullqt.
-# This may be replaced when dependencies are built.
--- imx6ullApp/build/build.sh
+++
@@ -1,6 +0,0 @@
-# 使用 -DCMAKE_TOOLCHAIN_FILE 指向你刚才写的工具链文件
-cmake .. -DCMAKE_TOOLCHAIN_FILE=../cmake/arm-qt-toolchain.cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-
-# 3. 编译
-# -j4 代表使用 4 个线程加速
-make -j4
